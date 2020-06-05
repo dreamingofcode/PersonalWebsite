0160e04e61fba9f06b313312448fb734fd0784ee
Cesar <cesarmota@ymail.com>
2020-06-05 18:57:36 -0500

    functional contact form with styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, Button } from 'react-mdl';
 import FadeIn from 'react-fade-in';
 import contactAvatar from '../assets/icons/contactAvatar.jpg';
 import textBox from '../assets/icons/textBoxx.png';
 
 const Contact = () => {
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }}>
+    <div>
       <FadeIn transitionDuration="1800">
-        <h1>Contact!</h1>
-        <FadeIn transitionDuration="7800">
-        
-        </FadeIn>
-        <Grid>
-          <Cell col={3}>
-            <FadeIn transitionDuration="11800">
-              <h2
-                style={{
-                  marginBottom: '-90px',
-                  // marginTop: '-550px',
-                  marginLeft: '330px',
-                }}
-              >
-                Hello!
-              </h2>
-            </FadeIn>
+        <h1>Contact Me!</h1>
+        <Cell col={8} offset="2">
+          <h5 style={{ marginTop: '-10px', marginBottom: '80px' }}>
+          You can Find hyperlinks to my social media accounts and my resume down below. For a faster response , I can be reached via telephone at (773)-664-5734 
+          </h5>
+        </Cell>
+        <Grid
+          className="about"
+          style={{
+            backgroundColor: 'white',
+            color: 'black',
+            paddingTop: '80px',
+            paddingBottom:"80px"
+          }}
+        >
+          <Cell col={3} phone={12} tablet={12}>
             <FadeIn transitionDuration="1800">
               <img src={contactAvatar} alt="cesar's avatar" height="400px" />
             </FadeIn>
@@ -36,76 +35,90 @@ const Contact = () => {
                 style={{
                   marginBottom: '0px',
                   marginTop: '-650px',
-                  marginLeft: '310px',
+                  marginLeft: '280px',
                   transform: 'scaleX(-1)',
                 }}
               />
+              <h2
+                style={{
+                  marginTop: '-380px',
+                  marginLeft: '305px',
+                }}
+              >
+                Hello!
+              </h2>
             </FadeIn>
           </Cell>
 
           <Cell
-            offset={0}
-            className="contactForm"
+            shadow={10}
+            offsetDesktop={2}
+            className="contactForm "
             col={6}
-            phone="12"
+            phone={12}
             tablet={12}
           >
-            <form  method="POST" name="contact">
-            <input type="hidden" name="form-name" value="contact" />
+            <form method="POST" name="contact">
+              <input type="hidden" name="form-name" value="contact" />
               <div className="fields">
-                <div className="field half">
-                  {/* <label>
-                    Your Name:{' '} */}
-                    <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder="Name"
-                    />
-                  {/* </label> */}
+                <div className="field half ">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Name"
+                    style={{
+                      width: '400px',
+                      marginBottom: '30px',
+                      height: '35px',
+                    }}
+                  />
                 </div>
                 <div className="field half">
-                  {/* <label>
-                    Your Email:{' '} */}
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                    />
-                  {/* </label> */}
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    style={{
+                      width: '400px',
+                      marginBottom: '30px',
+                      height: '35px',
+                    }}
+                  />
                 </div>
 
                 <div className="field">
                   {/* <label>
                     Message:{' '} */}
-                    <textarea
-                      name="message"
-                      id="message"
-                      placeholder="Message"
-                      rows="7"
-                    ></textarea>
+                  <textarea
+                    name="message"
+                    id="message"
+                    placeholder="Message"
+                    rows="7"
+                    style={{ width: '400px' }}
+                  ></textarea>
                   {/* </label> */}
                 </div>
-                {/* <div className="field"> */}
-                  {/* <label>
-                    Message:{' '} */}
-                    {/* <input
+                <div className="field">
+                  <label>
+                    Send File:{' '}
+                    <input
                       type="file"
                       name="myfile"
                       id="myfile"
                       placeholder="Upload File"
                       rows="7"
-                    /> */}
-                  {/* </label> */}
-                {/* </div> */}
-                <div className="field">
-                  <div data-netlify-recaptcha="true">
-                    
-                  </div>
+                    />
+                  </label>
+                </div>
+                <div className="field" style={{ marginBottom: '60px' }}>
+                  <div data-netlify-recaptcha="true"></div>
                 </div>
 
-                <button type="submit">Send</button>
+                <Button type="submit" raised ripple colored>
+                  Send
+                </Button>
               </div>
             </form>
           </Cell>
